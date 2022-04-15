5b0a1cdb00ed8504a74b5e8b1b150b724d33669b
Diego Artiles <diego45882@hotmail.com>
2022-04-15 20:23:40 -0300

    refactor: add more gap and links

--- components/link/LinkList.js
+++ components/link/LinkList.js
@@ -2,14 +2,16 @@ import { Container } from "@nextui-org/react"
 import * as SimpleIcons from "react-icons/si"
 import * as FaIcons from "react-icons/fa"
 import * as FeatherIcons from "react-icons/fi"
-import * as IonicIcons from "react-icons/io5"
+import * as Ionic5Icons from "react-icons/io5"
+import * as Ionic4Icons from "react-icons/io"
 import * as GrommetIcons from "react-icons/gr"
 import Link from "./Link"
 
 const Icons = {
     ...SimpleIcons,
     ...FaIcons,
-    ...IonicIcons,
+    ...Ionic5Icons,
+    ...Ionic4Icons,
     ...GrommetIcons,
     ...FeatherIcons
 }
@@ -27,7 +29,7 @@ function LinkList({ links }) {
             </Container>
         }
         {
-            links.some(link => link.primary) && <Container css={{ display: 'flex', flexDirection: 'row', maxWidth: 400, margin: '1rem 0', justifyContent: 'center' }}>
+            links.some(link => link.primary) && <Container css={{ display: 'flex', flexDirection: 'row', maxWidth: 400, margin: '1rem 0', justifyContent: 'center', columnGap: '$6' }}>
                     {
                         links
                             .filter(link => link.primary)
