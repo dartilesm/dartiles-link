977e20d780074dbdbb81f2396a38240f96213065
Diego Artiles <diego45882@hotmail.com>
2023-08-25 22:35:18 -0300

    get server url

--- __pages/api/links.js
+++
@@ -1,5 +0,0 @@
-import links from '../../links'
-
-export default function handler(req, res) {
-  res.status(200).json(links)
-}
--- src/utils/get-absolute-url.ts
+++ src/utils/get-absolute-url.ts
@@ -1,9 +1,16 @@
 // utils/getURL.ts
 const IS_SERVER = typeof window === 'undefined';
 
+function getServerUrl () {
+  const protocol = process.env.NODE_ENV === 'development' ? 'http' : 'https';
+  const host = process.env.NEXT_PUBLIC_VERCEL_URL || process.env.VERCEL_URL || 'localhost:3000';
+
+  return `${protocol}://${host}`;
+}
+
 export default function getAbsoluteUrl (path: string) {
   const baseURL = IS_SERVER
-    ? process.env.NEXT_PUBLIC_VERCEL_URL!
+    ? getServerUrl()
     : window.location.origin;
   return new URL(path, baseURL).toString();
 }
