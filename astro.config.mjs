6757e04a89b0627fa5e45ed9f26447f62fc4d30f
Diego Artiles <diego45882@hotmail.com>
2023-08-27 12:34:38 -0300

    add redirects to all enviroments

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,12 @@ import tailwind from "@astrojs/tailwind";
 import { defineConfig } from 'astro/config';
 import linkRedirects from './src/utils/link-redirects';
 import vercel from "@astrojs/vercel/serverless";
-const isDevelopment = import.meta.env.VITE_IS_DEVELOPMENT;
 
 /** @type {import('astro').AstroUserConfig} */
 
 // https://astro.build/config
 export default defineConfig({
-  redirects: isDevelopment ? linkRedirects : undefined,
+  redirects: linkRedirects,
   integrations: [tailwind(), react()],
   output: "server",
   adapter: vercel()
