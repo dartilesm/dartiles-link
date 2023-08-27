dd40f6a4c2529c99db4876098ba4d1c03e24c226
Diego Artiles <diego45882@hotmail.com>
2023-08-27 11:06:27 -0300

    restore link redirects from astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from 'astro/config';
+import linkRedirects from './src/utils/link-redirects';
 
 /** @type {import('astro').AstroUserConfig} */
 export default defineConfig({
+  redirects: linkRedirects,
   integrations: [tailwind(), react()],
 });
