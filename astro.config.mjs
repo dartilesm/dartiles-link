3c76a2d881b7570c250716855f41ab5f1e727c20
Diego Artiles <diego45882@hotmail.com>
2023-08-27 12:27:15 -0300

    add conditional redirects on development

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from 'astro/config';
+import linkRedirects from './src/utils/link-redirects';
+
+const isDevelopment = import.meta.env.VITE_IS_DEVELOPMENT;
 
 /** @type {import('astro').AstroUserConfig} */
 export default defineConfig({
+  redirects: isDevelopment ? linkRedirects : undefined,
   integrations: [tailwind(), react()],
 });
