5a15e903745fdd48032da730b3f177b8a1e79e50
Diego Artiles <diego45882@hotmail.com>
2023-08-27 12:33:49 -0300

    add vercel adapter and remove middleware

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,15 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from 'astro/config';
 import linkRedirects from './src/utils/link-redirects';
-
+import vercel from "@astrojs/vercel/serverless";
 const isDevelopment = import.meta.env.VITE_IS_DEVELOPMENT;
 
 /** @type {import('astro').AstroUserConfig} */
+
+// https://astro.build/config
 export default defineConfig({
   redirects: isDevelopment ? linkRedirects : undefined,
   integrations: [tailwind(), react()],
+  output: "server",
+  adapter: vercel()
 });
