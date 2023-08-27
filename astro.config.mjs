6181ca72e5b56b5d13f3d44f4b758d26c3e66570
Diego Artiles <diego45882@hotmail.com>
2023-08-26 22:24:48 -0300

    remove unused plugins

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,10 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import { nodeResolve } from '@rollup/plugin-node-resolve'
-import commonjs from '@rollup/plugin-commonjs';
 import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
 import linkRedirects from './src/utils/link-redirects';
 
 /** @type {import('astro').AstroUserConfig} */
 export default defineConfig({
   redirects: linkRedirects,
   integrations: [tailwind(), react()],
-  vite: {
-    plugins: [
-      commonjs({
-        include: /node_modules/,
-        extensions: ['.esm.js', '.js']
-      }),
-      nodeResolve()
-    ]
-  }
 });
